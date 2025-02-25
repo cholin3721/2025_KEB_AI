655e07ff1221388000cc241f4e06411b676217cd
cholin3721 <cholin3721@gmail.com>
2025-02-25 17:37:02 +0900

    v0.7 day 12 2nd assignment answer

--- day12.py
+++ day12.py
@@ -1,15 +1,20 @@
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import accuracy_score
 
-df = pd.DataFrame({
-    'A' : [1, 2, np.nan, 4],
-    'B' : [np.nan, 2, 3, 4],
-    'c' : [1, 2, 3, 4]
-})
-
-print(df)
-print()
+# df = pd.DataFrame({
+#     'A' : [1, 2, np.nan, 4],
+#     'B' : [np.nan, 2, 3, 4],
+#     'c' : [1, 2, 3, 4]
+# })
+#
+# print(df)
+# print()
 
 # 사이킷런을 사용하지 않고 산술평균으로 채워넣는 방법
 
@@ -24,4 +29,28 @@ print()
 
 # df = df.replace(np.nan, df.mean())
 # print(df)
+
+titanic = sns.load_dataset('titanic')
+
+print(titanic.info())
+titanic.dropna(subset = ["age"], inplace = True)
+
+X = titanic[['age']]
+y = titanic[['survived']]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 42)
+
+model = KNeighborsRegressor(n_neighbors = 5)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+plt.figure(figsize=(5,2))
+plt.scatter(X_test, y_test, color = 'blue', label = 'Real')
+plt.scatter(X_test, y_pred, color = 'red', label = 'Predicted')
+plt.title('KNeighborsRegressor: Real vs Predicted')
+plt.xlabel('Age')
+plt.ylabel('Survived')
+plt.show()
 
